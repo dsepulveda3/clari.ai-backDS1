b484cea303a3a7f43950d738591df4b2aaeb6457
brian <blsaavedra@uc.cl>
2023-03-08 16:29:23 -0300

    added pre_prompt logic to question endpoint

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,18 +8,18 @@ class QuestionsController < ApplicationController
 
   def count
     count = Question.count
-    render json: {'count': count}
+    render json: {count: count, status: :success}
   end
 
   def create
     question = Question.create(content: params[:content])
     lang = params[:lang]
-    pre_prompt_es = "Si es un ejercicio de matematicas o fisica explique como resolverlo teoricamente, pongame un ejemplo y luego resuelva el ejemplo y la pregunta paso a paso.
-    De lo contrario, si es una pregunta conceptual, explique en términos simples, detallados, definiendo primero los conceptos difíciles que se utilizarán en la explicación."
-    #pre_prompt_en = "Answer the following question, with an explanation easy for a student to understand:"
-    pre_prompt_en = "If this is a math or physics exercise explain how to solve it theoretically, give me an example and then solve the example and the question step by step.
-    Otherwise, if it is a conceptual question explain in simple terms, detailed, first defining difficult concepts that will be used in the explanation."
-    final_prompt = lang == "es" ? pre_prompt_es : pre_prompt_en
+    attempt = params[:attempt]
+    pre_prompts_es = ["Si es un ejercicio de matematicas o fisica explique como resolverlo teoricamente, pongame un ejemplo y luego resuelva el ejemplo y la pregunta paso a paso.
+    De lo contrario, si es una pregunta conceptual, explique en términos simples, detallados, definiendo primero los conceptos difíciles que se utilizarán en la explicación. ", "pre prompt 2 es. ", "pre prompt 3 es. "]
+    pre_prompts_en = ["If this is a math or physics exercise explain how to solve it theoretically, give me an example and then solve the example and the question step by step.
+    Otherwise, if it is a conceptual question explain in simple terms, detailed, first defining difficult concepts that will be used in the explanation. ", "pre prompt 2 en. ", "pre prompt 3 en. "]
+    final_prompt = lang == "es" ? pre_prompts_es[attempt] : pre_prompts_en[attempt]
     prompt = final_prompt + params[:content]
     openai_client = OpenAI::Client.new(api_key: ENV["API_KEY"], default_engine: "text-davinci-003")
     r = openai_client.completions(
@@ -28,8 +28,7 @@ class QuestionsController < ApplicationController
       temperature: 0.5,
       n: 1,
     )
-    puts("Heeey there")
-    question.update(answer: r[:choices][0][:text].lstrip)
+    question.update(answer: r[:choices][0][:text].lstrip, prompt: final_prompt)
     render json: {info: question, status: :success}
   end
 
